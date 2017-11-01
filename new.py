4b695f035036f1970afb0b6ffeb47cbe6c83e1e3
303PC02 <sandrastokka1@gmail.com>
2017-11-01 20:36:43 +0100

    Using user intpur to find circumference of a circle

---
+++ new.py
@@ -0,0 +1,12 @@
+#here we want to take an input from the user
+
+print ("Please enter the radius of the circle:")
+radius = float(input())
+print ("you have entered a radius of", radius)
+
+myConstPi = 3.14
+
+
+
+print("the circumference of a circle is :", 2*myConstPi*radius)
+
