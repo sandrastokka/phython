0a511ac32acef3d8d94b58424ad2aea06d7a97d2
303PC02 <sandrastokka1@gmail.com>
2017-11-01 20:19:49 +0100

    Kodekurs 1 jeg gjorde det fordi jeg skulle lære. Git it.

---
+++ funwithvariables.py
@@ -0,0 +1,12 @@
+
+#this is a variable
+radius = 10
+
+#this is a constant
+myConstPi = 3.14
+
+print("the radius of the circle is", radius)
+
+print("the circumference of a circle is:", 2*myConstPi*radius)
+
+
