e039374176cd92805eac6aa21a192469d328cc6b
sandrastokka <sandrastokka1@gmail.com>
2017-11-29 20:57:31 +0100

    Har startet å lage en firkant basert på tempratur celcius to fahrenheit

--- lagefunksjon.py
+++ lagefunksjon.py
@@ -1,19 +1,19 @@
 #defining a function
-def farenheitToCelcius():
-    fahrenheit = int (input ("What fahrenheit temperature do you want to convert? :"))
-    print
-    celcius =((fahrenheit -32/9)*5)
-    print( fahrenheit, celcius , "celcius")
-
-
-
-
-optionSelected = input ("do yo want to convert from celcius to fahrenheit or fahrenheit to celcius? write C for celcius to fahrenheit and F for fahrenheit to celcius"
-       "")
-print
-
-if(optionSelected == "f"):
-    farenheitToCelcius()
-
 import turtle
 
+def fahrenheitToCelcius():
+    myInput = int (input ("What temperature do you want to convert? :"))
+    tempInCelcius =((MyInput -32/9)*5)
+    print( "The temp of", myInput, "in fahrenheit is"fahrenheit, celcius , "celcius")
+
+
+
+(optionSelected = input ("do yo want to convert from celcius to fahrenheit or fahrenheit to celcius? write C for celcius to fahrenheit and F for fahrenheit to celcius")
+print
+
+if(optionSelected == "c"):
+fahrenheitToCelcius()
+elif(optionSelected == "f"):
+
+
+myTurtle=turtle.Turtle
