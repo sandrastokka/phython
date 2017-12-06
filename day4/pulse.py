8fb6f926073f912e3d3070b8c09e1432c067804d
sandrastokka <sandrastokka1@gmail.com>
2017-12-06 20:53:35 +0100

    Drawing a pulse with a list of temperatures and the turtle function

--- day4/pulse.py
+++ day4/pulse.py
@@ -16,9 +16,10 @@ def setupTurtle():
 myTurtle = setupTurtle()
 
 #define the temperature list
-averageTemperatureList = [3, 4, 6, 9, 14, 17, 18, 200, 17, 14, 11, 7, 4]
+averageTemperatureList = [3, 4, 6, 9, 14, 17, 18, 17, 14, 11, 7, 4]
+numberOfRainyDays = [22, 19, 19, 18, 17, 18, 19, 19, 20, 21, 21, 20]
 
-def pulse():
+def sandrapulse():
     for i in range(0, len(averageTemperatureList)):
         myTurtle.left(90)
         myTurtle.forward(averageTemperatureList[i])
@@ -29,5 +30,26 @@ def pulse():
         myTurtle.left(90)
         myTurtle.forward(20)
 
-pulse()
+
+
+
+
+def pulse(height,width):
+    myTurtle.left(90)
+    myTurtle.forward(height*10)
+    myTurtle.right(90)
+    myTurtle.forward(width)
+    myTurtle.right(90)
+    myTurtle.forward(height*10)
+    myTurtle.left(90)
+    myTurtle.forward(20)
+
+for temp in averageTemperatureList:
+    pulse(temp,25)
+
+myTurtle = setupTurtle()
+
+for temp in numberOfRainyDays:
+    pulse(temp,25)
+
 turtle.done()
