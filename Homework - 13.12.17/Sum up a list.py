ab83e0e4cae9626d6811054879010153f147937d
sandraabelsnesstokka <sandrastokka1@gmail.com>
2017-12-11 18:04:32 +0100

    Summing up a list - working with the understanding of lists and functions

--- Homework - 13.12.17/Sum up a list.py
+++ Homework - 13.12.17/Sum up a list.py
@@ -1,7 +1,9 @@
 
-
+#Write a Python function to sum all the items in a list.
+#define a list
 temperatureThisWeek  = [1, 2, 3, 4, 5, 6, 7]
 
+#make a function that sum all the items in the list
 def addTemperatureThisWeek():
     sumOfList = 0
     print ("summing up the temprature of this week")
