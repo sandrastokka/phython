290314f3c5025f06452793debe2ef0aa4806ca38
sandraabelsnesstokka <sandrastokka1@gmail.com>
2017-12-11 18:42:33 +0100

    Drawing a star with turtle

---
+++ Homework - 13.12.17/Find the lagrest number in a list.py
@@ -0,0 +1,10 @@
+#Write a Python function to get the largest number from a list.
+
+temperatureThisWeek  = [1, 2, 4, 1, 1, 3, 2,]
+
+def findlargestnumber():
+    highest = max (temperatureThisWeek)
+    print "the highest temperature this week is", highest, "celsius degrees"
+
+
+findlargestnumber()
