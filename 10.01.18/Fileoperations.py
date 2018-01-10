9f489ea7928f355f92987602d880e0eeb1ccfe2e
sandrastokka <sandrastokka1@gmail.com>
2018-01-10 20:29:23 +0100

    Somthing is wrong, check it out :)

--- 10.01.18/Fileoperations.py
+++ 10.01.18/Fileoperations.py
@@ -2,12 +2,39 @@
 
 #reading a file
 
-def readMyDataFile():
-    with open("someDat.txt", mode='r')as myDataFile:
+def readMyDataFile(dataFileName):
+    with open(dataFileName, mode='r')as myDataFile:
         print (type(myDataFile))
         clubs = myDataFile.read().splitlines()
-        print(clubs)
+        return items
 
-readMyDataFile()
+#readMyDataFile()
+
+
+def writeDataToMyFile(text):
+    with open ("testing.txt",'w')as myDataFile:
+        for item in text:
+            myDataFile.write(item+'\n')
+
+text = ['a' ,'b', 'c', 'd', 'e', 'f']
+writeDataToMyFile(text)
+
+#somthing is wrong with the last part of the program, maybe the first as well
+
+def seeifItemExists(myItem):
+    myshoppinglist = readMyDataFile('testing.txt')
+    for item in myshoppinglist:
+        if item == myItem:
+            print (myItem, "Exist n my shopping list")
+            return
+
+    print (myItem, "Does not exist in my shopping list")
+
+
+    seeifItemExists("a")
+
+
+
+
 
 
